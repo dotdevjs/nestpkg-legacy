86ab70746282396f1677c7e01f2f2a555c57a4b4
Iv Po <me@dotdev.ru>
2020-11-18 20:35:59 +0200

    Fixes

--- packages/typeorm/src/type-orm.module.ts
+++ packages/typeorm/src/type-orm.module.ts
@@ -1,3 +1,26 @@
-import { TypeOrmModule as BaseTypeOrmModule } from '@nestjs/typeorm';
+import {
+  TypeOrmModule,
+  TypeOrmModule as BaseTypeOrmModule,
+} from '@nestjs/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
+import { DynamicModule } from '@nestjs/common';
+import { EntityProviderService } from './services';
 
-export class TypeormModule extends BaseTypeOrmModule {}
+export class TypeormModule extends BaseTypeOrmModule {
+  static forRoot(options?: TypeOrmModuleOptions): DynamicModule {
+    return TypeOrmModule.forRootAsync({
+      useFactory: (entityProvider: EntityProviderService) => {
+        const entities = [
+          ...entityProvider.getEntities(),
+          ...(options.entities || []),
+        ];
+
+        return {
+          ...options,
+          entities,
+        };
+      },
+      inject: [EntityProviderService],
+    });
+  }
+}
