3b55c91e839ebdec4fddc2228d8fa496541da8e9
Iv Po <me@dotdev.ru>
2020-11-25 01:36:19 +0200

    Fixes

--- packages/nest/crud-request/src/index.ts
+++ packages/nest/crud-request/src/index.ts
@@ -1,1 +1,12 @@
+import { validateComparisonOperator } from '@nestjsx/crud-request/lib/request-query.validator';
+
+const monkeypatch = require('monkeypatch');
+
+monkeypatch(validateComparisonOperator, 'validateComparisonOperator', function (
+  original: any
+) {
+  console.log('validateComparisonOperator');
+  return original();
+});
+
 export * from './types/request-query.types';
