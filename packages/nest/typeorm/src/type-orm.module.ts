9bff13584e54c8a55c5a88180243c88ea8e2cb59
Iv Po <me@dotdev.ru>
2020-11-25 19:27:09 +0200

    Fixes

--- packages/nest/typeorm/src/type-orm.module.ts
+++ packages/nest/typeorm/src/type-orm.module.ts
@@ -1,33 +1,59 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
-import { DynamicModule } from '@nestjs/common';
+import { DynamicModule, Logger } from '@nestjs/common';
 import { TypeOrmModule as BaseTypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
+import * as fs from 'fs';
+import * as path from 'path';
 
 import { UuidNormalizerSubscriber } from './subscribers/uuid-normalizer.subscriber';
 
 export class TypeOrmModule {
   static forRoot(options?: TypeOrmModuleOptions): DynamicModule {
     options = {
-      ...(options || {}),
+      ...options,
       subscribers: TypeOrmModule.createDefaultSubscribers(options?.subscribers),
     };
     return BaseTypeOrmModule.forRoot(options);
   }
 
   static forTest(options?: TypeOrmModuleOptions): DynamicModule {
-    options = {
-      type: 'sqlite',
-      database: ':memory:',
-      autoLoadEntities: true,
-      dropSchema: true,
-      synchronize: true,
-      logging: false,
-      ...((options as any) || []),
-      subscribers: TypeOrmModule.createDefaultSubscribers(options?.subscribers),
-    };
+    try {
+      const ormConfigJSON = fs
+        .readFileSync(path.join(process.cwd(), 'ormconfig.json'))
+        .toString();
+      const ormConfig = JSON.parse(ormConfigJSON);
 
-    return BaseTypeOrmModule.forRoot(options);
+      options = {
+        ...ormConfig,
+        ...options,
+        subscribers: TypeOrmModule.createDefaultSubscribers(
+          options?.subscribers
+        ),
+      };
+
+      Logger.debug('[TypeOrmModule] ormconfig.json found.');
+
+      return BaseTypeOrmModule.forRoot(options);
+    } catch (e) {
+      options = {
+        type: 'sqlite',
+        database: ':memory:',
+        autoLoadEntities: true,
+        dropSchema: true,
+        synchronize: true,
+        logging: false,
+        ...((options as any) || []),
+        subscribers: TypeOrmModule.createDefaultSubscribers(
+          options?.subscribers
+        ),
+      };
+
+      Logger.error(e);
+      Logger.debug('[TypeOrmModule] Fallback to sqlite.');
+
+      return BaseTypeOrmModule.forRoot(options);
+    }
   }
 
   private static createDefaultSubscribers(subscribers: (Function | string)[]) {
--- packages/nest/typeorm/test/type-orm.module.spec.ts
+++ packages/nest/typeorm/test/type-orm.module.spec.ts
@@ -3,5 +3,7 @@ import { TypeOrmModule } from '@nestpkg/typeorm';
 describe('TypeOrmModule', () => {
   it('should be defined', () => {
     expect(TypeOrmModule).toBeDefined();
+    expect(() => TypeOrmModule.forRoot()).not.toThrow();
+    expect(() => TypeOrmModule.forTest()).not.toThrow();
   });
 });
