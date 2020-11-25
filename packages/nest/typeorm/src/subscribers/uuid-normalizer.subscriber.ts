ebcdc8d914b22f4c5eeac3cb394f7d39cb2bbbc8
Iv Po <me@dotdev.ru>
2020-11-25 05:55:02 +0200

    Fixes

--- packages/nest/typeorm/src/subscribers/uuid-normalizer.subscriber.ts
+++ packages/nest/typeorm/src/subscribers/uuid-normalizer.subscriber.ts
@@ -3,6 +3,6 @@ import { EventSubscriber, InsertEvent } from 'typeorm';
 @EventSubscriber()
 export class UuidNormalizerSubscriber {
   beforeInsert(event: InsertEvent<any>) {
-    console.log(`BEFORE POST INSERTED: `, event);
+    //console.log(`BEFORE POST INSERTED: `, event);
   }
 }
