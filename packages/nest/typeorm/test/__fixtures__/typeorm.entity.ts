423cf2047c2285e6db8a10e3a5d22f6206ddfbbb
Iv Po <me@dotdev.ru>
2020-12-01 12:40:51 +0200

    chore(nest-typeorm): Fixes

--- packages/nest/typeorm/test/__fixtures__/typeorm.entity.ts
+++ packages/nest/typeorm/test/__fixtures__/typeorm.entity.ts
@@ -1,5 +1,5 @@
-import { PrimaryGeneratedColumn } from 'typeorm';
 import { Entity } from '@nestpkg/typeorm';
+import { PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class TypeOrmEntity {
