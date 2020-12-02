9f6200914d4736204884da96666b89166b50474a
Iv Po <me@dotdev.ru>
2020-12-02 09:24:53 +0200

    Fixes

--- packages/nest/typeorm/test/subscriber/after-load-many.spec.ts
+++ packages/nest/typeorm/test/subscriber/after-load-many.spec.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
+import { Connection, EventSubscriber } from 'typeorm';
 import { NestApplicationContext } from '@nestjs/core';
 import { Test } from '@nestjs/testing';
-import { getRepositoryToken, TypeOrmModule } from '@nestpkg/typeorm';
-import { EventSubscriber } from 'typeorm';
+import { TypeOrmModule } from '@nestpkg/typeorm';
 
 import { TypeOrmEntity } from '../__fixtures__/typeorm.entity';
 
-describe('after-load-many', () => {
+describe('AfterLoadMany', () => {
   @EventSubscriber()
   class AfterLoadManySubscriber {
     afterLoadMany() {}
@@ -16,16 +16,19 @@ describe('after-load-many', () => {
 
   let moduleRef: NestApplicationContext;
 
-  const AfterLoadManyOrmModule = TypeOrmModule.forFeature([TypeOrmEntity]);
-
   beforeAll(async () => {
     moduleRef = await Test.createTestingModule({
-      imports: [TypeOrmModule.forTest(), AfterLoadManyOrmModule],
+      imports: [
+        TypeOrmModule.forTest({
+          entities: [TypeOrmEntity],
+          subscribers: [AfterLoadManySubscriber],
+        }),
+      ],
     }).compile();
   });
 
   it('should listen "afterLoadMany" event', async () => {
-    const repository = moduleRef.get(getRepositoryToken(TypeOrmEntity));
+    const repository = moduleRef.get(Connection).getRepository(TypeOrmEntity);
     await repository.save(new TypeOrmEntity());
     await repository.find();
     expect(AfterLoadManySubscriber.prototype.afterLoadMany).toHaveBeenCalled();
