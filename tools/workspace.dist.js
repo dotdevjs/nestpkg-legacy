608f6114f65ddd3e82ab378c5e6d98bff199dfa6
Iv Po <me@dotdev.ru>
2021-02-23 09:13:45 +0200

    Fixes

--- tools/workspace.dist.js
+++ tools/workspace.dist.js
@@ -5,18 +5,27 @@ const workspaceConfigPath = path.join(__dirname, '..', 'workspace.json');
 const configDataJSON = fs.readFileSync(workspaceConfigPath).toString();
 const config = JSON.parse(configDataJSON);
 
+const version = config.version || 1
+
 Object.keys(config.projects).forEach((project) => {
   const projectConfig = config.projects[project];
-  const build = projectConfig.architect.build.options || {};
+  const keyName = version === 1 ? 'architect' : 'targets';
+  const build = projectConfig[keyName].build.options || {};
+
   if (!build.outputPath) {
+    console.log(`Empty outputPath project ${project}.`)
     return;
   }
+
   const outputPath = build.outputPath.substring(0, 5);
   if ('dist/' !== outputPath) {
+    console.log(`Invalid dist project ${project}.`)
     return;
   }
+
   const newOutputPath = build.outputPath.substring(5) + '/dist';
-  projectConfig.architect.build.options.outputPath = newOutputPath;
+  projectConfig[keyName].build.options.outputPath = newOutputPath;
+
   config.projects[project] = projectConfig;
 });
 
@@ -25,6 +34,7 @@ try {
 } catch (e) {
   console.warn('Backup file not exists.');
 }
+
 fs.copyFileSync(workspaceConfigPath, workspaceConfigPath + '.bak');
 fs.writeFileSync(workspaceConfigPath, JSON.stringify(config, null, 2));
 console.log('Success.');
