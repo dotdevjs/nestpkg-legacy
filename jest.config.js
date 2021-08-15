a66db038161ba71c5a2103efdc89030184d85c5c
Iv Po <me@dotdev.ru>
2021-08-15 22:13:11 +0300

    Fixes

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
     '<rootDir>/packages/nest/crud',
     '<rootDir>/packages/nest/core',
     '<rootDir>/packages/nest/typeorm',
-    '<rootDir>/packages/react/inversify',
     '<rootDir>/packages/nest/crud-typeorm',
     '<rootDir>/packages/nest/crud-request',
     '<rootDir>/packages/nest/console',
